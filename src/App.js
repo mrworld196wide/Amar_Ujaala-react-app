11a9803cc44cac23e9555beb6c24f3c5204140b3
mrworld196wide <shubhum19@gmail.com>
2023-06-11 10:05:12 +0530

    API fetched and Previous & Next Buttons added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import './App.css';
 
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
-import News from './components/News.js';
+import { News } from './components/News';
+
 
 export default class App extends Component {
   render() {
