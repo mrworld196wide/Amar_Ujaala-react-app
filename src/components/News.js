d7a3bc40c7b8799e587fe7e4bad4d98480591d73
mrworld196wide <shubhum19@gmail.com>
2023-06-12 17:11:41 +0530

    Adding time, author and news source to News App

--- src/components/News.js
+++ src/components/News.js
@@ -77,13 +77,13 @@ export class News extends Component {
           <div className="row">
             {!this.state.loading && this.state.articles.map((element) => {
               return <div className="col-md-4" key={element.url}>
-                <NewsItem title={element.title ? element.title : ""} description={element.description ? element.description : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
               </div>
             })}
           </div>
           <div className="container d-flex justify-content-between">
             <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
-            <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults / this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+            <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
           </div>
         </div>
       </>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,17 +2,23 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
   render() {
-    let { title, description, imageUrl, newsUrl  } = this.props
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
       <>
         <div className="container my-3">
           <div className="card" >
-            <img src={imageUrl} className="card-img-top" alt="..." />
+            <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '90%', zIndex: '1' }}> {source}
+            </span>
+            <img src={!imageUrl ? "https://images.unsplash.com/photo-1628595351029-c2bf17511435?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1332&q=80" : imageUrl}
+             className="card-img-top" alt="..." />
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
+              <p className="card-text">
+                <small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small>
+              </p>
               {/* target='_blank' opens the url in new tab */}
-              <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>    
+              <a href={newsUrl} target='_blank' rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
             </div>
           </div>
         </div>
